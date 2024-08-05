9adffc73a4cfd3cbcee446bde3e97f685d4f860c
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-08-05 06:11:44 +0300

    5. Get profile picture data from Telegram

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,23 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { total_count, photos } = await bot.api.getUserProfilePhotos(id);
+  // Photos are all of user's photos. We want the first one.
+
+  if (total_count !== 0) {
+    const currentPhoto = photos[0];
+    // currentPhoto is an array of the same photo in different resolutions. We want the last one.
+
+    const bestLookingSelfie = currentPhoto[currentPhoto.length - 1];
+
+    res.json(bestLookingSelfie);
+  }
+
+  res.json({ src: "" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
