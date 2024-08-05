4e2d45a7e6aa9331ff3f15de2e14c47b2905ddfa
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-08-05 05:52:41 +0300

    3. Finish frontend

--- frontend/src/components/Guide.tsx
+++ frontend/src/components/Guide.tsx
@@ -1,12 +1,26 @@
 import { useInitData } from "@telegram-apps/sdk-react";
 import { Avatar, Subheadline } from "@telegram-apps/telegram-ui";
+import { useEffect, useState } from "react";
 
 export default function Guide() {
   const initData = useInitData();
+  const initialValue = initData?.user?.photoUrl; // Naive, isn't it?
+  const [src, setSrc] = useState<string | undefined>(initialValue);
+
+  useEffect(() => {
+    const user = initData?.user;
+    const fetchProflePicture = async () => {
+      const response = await fetch(`http://localhost:3000/${user?.id}`);
+      const data = await response.json();
+      setSrc(data.result);
+    };
+
+    fetchProflePicture();
+  }, [initData]);
 
   return (
     <article>
-      <Avatar src="TODO" />
+      <Avatar src={src} />
       <Subheadline>{initData?.user?.firstName}</Subheadline>
     </article>
   );
